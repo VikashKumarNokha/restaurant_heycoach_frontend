2fd792865e08da89985d6808f76d35689e62ac5d
VikashKumarNokha <vikashkumarnokha@gmail.com>
2023-11-09 15:59:36 +0530

    In homepage data showed

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,26 +6,37 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-export default function MediaCard() {
+export default function MediaCard({restaurantdata}) {
+
+    console.log("dddd",restaurantdata)
+     
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
+        image={restaurantdata?.image_url}
         title="green iguana"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          {restaurantdata?.name}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          Addresss :  {restaurantdata?.address}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Email Id :  {restaurantdata?.email}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Mobile :  {restaurantdata?.mobile}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Added By :  {restaurantdata?.User?.fullName}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+        <Button size="small">Delete</Button>
+        <Button size="small">Update</Button>
       </CardActions>
     </Card>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import MediaCard from '../components/Card'
+import { baseurl } from '../utilities/BaseUrl'
+import axios from 'axios'
 
 function HomePage() {
+    const [restaurantData, setRestaurantData] = useState([]);
+
+    useEffect(()=>{
+        getRestaurantlist();
+   },[ ])
+
+  function getRestaurantlist(){  
+       return  axios.get( baseurl + "restaurants").then((res)=>{
+             console.log("resss",res)
+             setRestaurantData( res?.data)
+       }).catch((err)=>{
+          console.log("err", err);
+       })
+  }
+
+     
+
+
   return (
-    <div>HomePage</div>
+    <div>
+        {/* Navbar componenet for home page  */}
+        <Navbar/>
+        {/*navber end here  */}
+       
+        <div style={{display : "flex", justifyContent : "space-evenly", flexWrap :"wrap", margin : "10px"}}>
+        {
+            restaurantData.length > 0 && restaurantData.map((e)=>{
+                 return   <div key={e?.id} style={{margin : "10px"}}  >
+                            <MediaCard restaurantdata={e} />
+                          </div>
+            })
+        }
+        </div>
+
+        <Footer/>
+    </div>
   )
 }
 
