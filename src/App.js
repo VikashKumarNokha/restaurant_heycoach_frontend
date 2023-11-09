ed041fb2428460316913b01d0246c020d74b0874
VikashKumarNokha <vikashkumarnokha@gmail.com>
2023-11-09 15:20:15 +0530

    routes, homepage, Addrestaurantpage, UpdateRestaurannt page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import HomePage from './pages/HomePage';
+import AddRestaurantPage from './pages/AddRestaurantPage';
+import UpdateRestaurantPage from './pages/UpdateRestaurantPage';
 
 function App() {
   return (
     <div className="App">
-        hello client
+        <BrowserRouter>
+       <Routes>
+          <Route path="/" element={<HomePage/>  } />
+          <Route path="/updaterestarant" element={<UpdateRestaurantPage/>  } />
+          <Route path="/addrestarant" element={<AddRestaurantPage/>  } />
+       </Routes>
+      </BrowserRouter>  
     </div>
   );
 }
---
+++ src/pages/AddRestaurantPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddRestaurantPage() {
+  return (
+    <div>AddRestaurantPage</div>
+  )
+}
+
+export default AddRestaurantPage
